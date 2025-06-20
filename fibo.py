5c982a406122d3fab41776a24a28f873cbcc9c28
ayushisoni-28 <ayushisoni2813@gmail.com>
2025-06-21 01:20:47 +0530

    practice repo

---
+++ fibo.py
@@ -0,0 +1,53 @@
+#generate infinite fibonaacci series 
+'''def fib(n):
+    a,b=0,1
+    count=0
+    while count<n:
+        yield a
+        a,b=b,a+b
+        count+=1
+
+for i in fib(5):
+    print(i,end=' ')'''
+
+
+
+def fib_():
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b, a+b
+
+
+res=fib_()
+for _ in range(5):
+    print(next(res))
+
+
+
+
+'''
+
+stp by stp exec fibonaacciii
+
+
+
+
+1. def fun banaegeeeeee
+
+2. initalize kareeegeeeee 1,0 wali val k var koooooo
+
+3. while true / while condition lagege
+
+4.increment 
+
+
+
+5. fun calllllll
+range k liye for loop
+
+
+range set kareeeeee
+
+
+'''
